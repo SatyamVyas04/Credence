ab0da165f8e5e19e33bba7f6fdd464bc345304ec
SatyamVyas04 <vyas.satyam.04@gmail.com>
2025-03-05 15:50:14 +0530

    Add a design page to implement proper typography

---
+++ app/design/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+
+      <p>This is a regular paragraph text.</p>
+
+      <blockquote>This is a blockquote element</blockquote>
+
+      <ul>
+        <li>Unordered list item 1</li>
+        <li>Unordered list item 2</li>
+      </ul>
+
+      <ol>
+        <li>Ordered list item 1</li>
+        <li>Ordered list item 2</li>
+      </ol>
+
+      <strong>Bold text</strong>
+      <br />
+      <em>Italic text</em>
+      <br />
+      <small>Small text</small>
+    </div>
+  );
+}
